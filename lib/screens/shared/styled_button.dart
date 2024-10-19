5f7856f406c66f8354f97e85451616d384607b0e
Marcin <marcdud3k@gmail.com>
2024-10-19 23:46:19 +0200

    feat: Add User:Auth

--- lib/screens/shared/styled_button.dart
+++ lib/screens/shared/styled_button.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyledButton extends StatelessWidget {
-  const StyledButton({super.key});
+  const StyledButton({super.key, required this.onPressed, required this.child});
+
+  final void Function() onPressed;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)))),
+        onPressed: onPressed,
+        child: child);
+  }
+}
+
+class StyledButtonText extends StatelessWidget {
+  const StyledButtonText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: GoogleFonts.poppins(
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0.8,
+          ),
+        ));
   }
 }
