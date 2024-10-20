9f1515a91ad3be81df17f960767a3581b089fddd
Marcin <marcdud3k@gmail.com>
2024-10-20 16:47:57 +0200

    feat: Add:NavigationBar

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application/models/app_user.dart';
+import 'package:flutter_application/screens/shared/styled_button.dart';
+import 'package:flutter_application/screens/shared/styled_text.dart';
+import 'package:flutter_application/services/auth_service.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key, required this.user});
+
+  final AppUser user;
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const StyledHeading("Profile"),
+            const SizedBox(
+              height: 16.0,
+            ),
+
+            // output email
+            StyledBodyText("Witaj, ${widget.user.email}"),
+            const SizedBox(height: 15.0),
+
+            StyledButton(
+                onPressed: () {
+                  AuthService.singOut();
+                },
+                child: const StyledButtonText("Wyloguj się")),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+              backgroundColor: Colors.transparent,
+              color: Colors.black,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.black,
+              gap: 8,
+              onTabChange: (index) {
+                print(index);
+              },
+              padding: EdgeInsets.all(16),
+              tabs: const [
+                GButton(icon: Icons.home, text: "Home"),
+                GButton(icon: Icons.notifications, text: "Notifications"),
+                GButton(icon: Icons.person, text: "Profile"),
+                GButton(icon: Icons.settings, text: "Settings"),
+              ]),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/welcome/sign_in.dart
+++ lib/screens/welcome/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/screens/shared/styled_button.dart';
 import 'package:flutter_application/screens/shared/styled_text.dart';
+import 'package:flutter_application/services/auth_service.dart';
 import 'package:flutter_application/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,6 +17,8 @@ class _SignInFormState extends State<SignInForm> {
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  String? _errorFeedback;
 
   @override
   Widget build(BuildContext context) {
@@ -103,15 +106,30 @@ class _SignInFormState extends State<SignInForm> {
             const SizedBox(height: 16.0),
 
             // error feedback
+            if (_errorFeedback != null)
+              Text(_errorFeedback!, style: const TextStyle(color: Colors.red)),
 
             // sumbit button
             StyledButton(
               onPressed: () async {
+                setState(() {
+                  _errorFeedback = null;
+                });
+
                 // Daje ! na końcu bo wiem że formKey nie będzie posiadać wartowści null
                 if (_formKey.currentState!.validate()) {
                   // trim żeby pozbyć się pustej przestrzeni
                   final email = _emailController.text.trim();
                   final password = _passwordController.text.trim();
+
+                  final user = await AuthService.signIn(email, password);
+
+                  // error feedback
+                  if (user == null) {
+                    setState(() {
+                      _errorFeedback = "Nieprawidłowe dane logowania";
+                    });
+                  }
                 }
               },
               child: const StyledButtonText("Zaloguj się"),
